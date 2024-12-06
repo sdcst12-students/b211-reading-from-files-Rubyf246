f52ae8cd30225459aad5c7ff179585d83ab3271c
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2024-12-05 19:19:12 -0800

    done task1 and task3

--- task1.py
+++ task1.py
@@ -8,18 +8,23 @@ The return value is the line number (starting at 0) that the parameter to be fou
 Example:
 assert find('apple') == 0
 assert find('fish') == 5
+
+ 
 '''
 
 def find(needle):
     datatask1 = 'task01.txt'
     data1 = open(datatask1,'r')
     data = data1.read()
-    print (data)
-
-
-    pass
+    with open(datatask1) as fd:
+        for n, line in enumerate(fd):
+            s = line.strip()
+            if s == needle:
+                return n    
+    return -1
 
 
 if __name__ == "__main__":
+     
     assert find('apple') == 0
     assert find('fish') == 5
--- task3.py
+++ task3.py
@@ -7,3 +7,28 @@ Read the contents of task03.txt into your program and determine the points value
 
 For sample data task03.txt, the largest sum should be 68787
 """
+
+def sum():
+    list_sum = []
+    sum = 0
+    datatask1 = 'task03.txt'
+    data1 = open(datatask1,'r')
+    data = data1.read()
+
+    myarray = data.split("\n\n")
+    for line in myarray:
+        sum = 0
+        myarray2 = line.split("\n")
+        for line2 in myarray2:
+            
+            #print(line2.strip("\n"))
+            if (line2.strip("\n") != ""):
+                sum = sum + int(line2.strip("\n"))  
+        
+        list_sum.append(sum)
+        #print(list_sum)
+        
+    return max(list_sum)   
+        
+if __name__ == "__main__":        
+    print("the largest sum is: " + str(sum()))
